04ddf03fddd81e839f88117f17fb2cd939f8aaf0
sz0moldan <sz0moldan@vasvari.org>
2021-09-21 11:23:19 +0200

    Név osztály létrejött

---
+++ SpectatorProject/SpectatorProject/Model/Name.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpectatorProject.Model
+{
+    /// <summary>
+    /// Név osztály
+    /// </summary>
+    class Name
+    {
+    }
+}
